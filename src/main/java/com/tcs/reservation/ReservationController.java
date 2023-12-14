de9014543eaf8ee1b5b91844763aaf96f7a195fa
Justin Chan <justin.chan.3719@gmail.com>
2023-12-14 14:06:37 -0500

    Working on booking a room using restTemplates

--- src/main/java/com/tcs/reservation/ReservationController.java
+++ src/main/java/com/tcs/reservation/ReservationController.java
@@ -7,14 +7,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1/reservations")
 public class ReservationController {
-	private ReservationRepository reservationRepository;
 
-	public ReservationController(ReservationRepository reservationRepository) {
+	private ReservationRepository reservationRepository;
+	private RestTemplate restTemplate;
+
+	public ReservationController(ReservationRepository reservationRepository, RestTemplate restTemplate) {
 		this.reservationRepository = reservationRepository;
+		this.restTemplate = restTemplate;
 	}
 
 	@PostMapping
@@ -28,5 +32,11 @@ public class ReservationController {
 		Reservation reservationDto = new Reservation(reservationById.getId(), reservationById.getCustomerId(),
 				reservationById.getHotelId(), reservationById.getStartDate(), reservationById.getEndDate());
 		return ResponseEntity.ok(reservationDto);
+	}
+
+	@PostMapping("/reserveHotel")
+	public String reserveHotel(@RequestBody Reservation reservation) {
+		return restTemplate.getForObject("http://localhost:8082/api/v1/hotelManagements/" + reservation.getHotelId(),
+				String.class);
 	}
 }
